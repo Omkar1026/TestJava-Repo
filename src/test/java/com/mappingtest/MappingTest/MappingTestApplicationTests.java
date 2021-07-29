1a994cf3e9eac379effa73e2e133a71b63a63729
Omkar1026 <omkarkarekar1998@gmail.com>
2021-07-29 19:31:11 +0530

    First Commit
    Signed-off-by: Omkar1026 <omkarkarekar1998@gmail.com>

---
+++ src/test/java/com/mappingtest/MappingTest/MappingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mappingtest.MappingTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
